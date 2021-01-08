d2fec6feed11cae9764250e8edd8522fea1504d4
muraoShanon <murao@shanon.co.jp>
2021-01-08 22:18:02 +0900

    not directは存在しないので考慮しないように変更

--- src/direct.ts
+++ src/direct.ts
@@ -2,9 +2,6 @@ import { Dokokara } from "./dokokara.types";
 import { DokokaraBuilder } from "./builder";
 
 export function direct(): Dokokara {
-  // if (referrer) return null;
-  // if (co) return null;
-
   return DokokaraBuilder({
     medium: "direct",
     source: "",
--- tests/direct.test.ts
+++ tests/direct.test.ts
@@ -1,18 +1,7 @@
 import { direct } from "../src/direct";
-import { getCampaignObject } from "../src/query";
 
 describe("direct", () => {
-  test("not direct1", () => {
-    expect(direct("www.example.com", null)).toBe(null);
-  });
-
-  test("not direct2", () => {
-    const qs = "?utm_source=newsletter&utm_medium=email&utm_campaign=spring_sale";
-    const co = getCampaignObject(qs);
-    expect(direct("", co)).toBe(null);
-  });
-
   test("direct", () => {
-    expect(direct("", null).channel).toBe("Direct");
+    expect(direct().channel).toBe("Direct");
   });
 });
